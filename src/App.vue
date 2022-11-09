026ecba93784631a5f0e023b40103fec457ae3c9
miao <15233722133@163.com>
2022-11-09 09:46:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- <div>{{name}}--{{age}}--{{like}}</div>
+  <div>toref:{{like}}</div>
+  <button @click="change">submit</button> -->
+  <!-- <input type="text" v-model="a" />
+  <input type="text" v-model="b" />
+  <p>{{ name }}</p> -->
+  <input id="btn" type="text" v-model="message1" />
+  <input type="text" v-model="message2" />
+</template>
+<script setup lang="ts">
+import {
+  watchEffect,
+  computed,
+  ref,
+  toRef,
+  reactive,
+  toRefs,
+  toRaw,
+  watch,
+} from "vue";
+// const man = reactive({name:'man',age:23,like:"jk"})
+// const {name,age,like} = toRefs(man)
+// // const like = toRef(man,'like')
+// const change =()=>{
+//   // man.like = 'llt'
+//   console.log(name,age,like)
+// }
+//toRef 只能修改响应式的值 非响应式试图毫无变化
+// let a = ref("");
+// let b = ref("");
+// const name = computed({
+//   get() {
+//     return a.value + b.value;
+//   },
+//   set() {
+//     a.value + b.value;
+//   },
+// });
+let message1 = ref<string>("");
+let message2 = ref<string>("");
+
+// watch(message,(n,o)=>{
+//   console.log('new',n,'old',o)//新的值和旧的值
+// },{
+//   deep: true,
+//   immediate: true,
+// })
+watchEffect(
+  (onincalidate) => {
+    // let btn: HTMLInputElement = document.querySelector(
+    //   "#btn"
+    // ) as HTMLInputElement;
+    console.log("massage1", message1.value);
+    // console.log("massage2", message2.value);
+    // console.log("input", btn);
+
+    onincalidate(() => {
+      console.log("before");
+    });
+  },
+  {
+    flush: "post", //配置这个选项可以获取dom
+    onTrigger(e) {
+      debugger;//调试  触发进入断点
+    },
+  }
+);
+</script>
+<style lang="less" scoped>
+/* .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+} */
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <button >修改</button>
+</template>
+<script setup lang="ts">
+import { customRef, ref, shallowRef, triggerRef } from "vue";
+function MyRef<T>(value: T) {
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track();
+        return value;
+      },
+      set(newVal) {
+        value = newVal;
+        trigger();
+      },
+    };
+  });
+}
+const count = ref(0);
+</script>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
