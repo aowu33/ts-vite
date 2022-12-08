27512fd53c847e7e798466af837b8108bb303f8e
miao <15233722133@163.com>
2022-12-08 14:33:10 +0800

    UI框架

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
 <template>
-  <!-- <LayOut></LayOut> -->
-  <div class="a">ddd</div>
+  <LayOut></LayOut>
 </template>
 
 <script lang="ts" setup>
 import LayOut from "./layout/index.vue";
 </script>
 
-<style lang="less">
-#app{
-    // padding: 0;
-    // margin: 0;
-    box-sizing: border-box;
-}
-.a{
-    width: 100%;
-    height: 100%;
-    background-color: aliceblue;
-}
-</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,34 +1,48 @@
 <template>
-<div class="common-layout">
+  <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-aside width="200px">
+        <el-header>Header</el-header>
+        Aside
+      </el-aside>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-header>Header</el-header>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style lang="less">
-// .common-layout{
-//     width: 100%;
-//     height: 100vh;
-//     margin: 0;
-//     // background-color: bisque;
-//     .el-container{
-//         width: 100%;
-//         background-color: #fff;
-//         .el-header{
-//             background-color: rgb(2, 23, 37);
-//             .el-aside{
-//                 background-color: rgb(61, 59, 59);
-//             }
-//         }
-//     }
-// }
+.common-layout {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  display: flex;
+  .el-container {
+    width: 100%;
+    background-color: #fff;
+    .el-aside {
+      height: 100vh;
+      color: #efefef;
+      background-color: rgb(25, 35, 48);
+      .el-header{
+        background-color: rgb(42, 64, 87)
+      }
+    }
+    .el-container {
+      background-color: #efefef;
+      .el-header {
+        background-color: rgb(204, 215, 226);
+      }
+      .el-main {
+        background-color: #fff;
+        margin: 20px;
+        border-radius: 2px;
+      }
+    }
+  }
+}
 </style>
