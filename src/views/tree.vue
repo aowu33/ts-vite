fd4cfe71395509aa7d84cbc435640b40805c844f
miao <15233722133@163.com>
2022-12-09 16:16:25 +0800

    高亮显示问题

--- src/views/tree.vue
+++ src/views/tree.vue
@@ -1,49 +1,75 @@
 <template>
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" default-expand-all>
-      <template #default="{ node, data }">     
-        <div class = "custom-tree-node" slot-scope = "{ node, data }">
+  <el-tree
+    :data="data"
+    :props="defaultProps"
+    highlight-current
+    @node-click="handleNodeClick"
+    current-node-key="key"
+    node-key="id"
+    default-expand-all
+  >
+    <template #default="{ node, data }">
+      <div class="custom-tree-node" slot-scope="{ node, data }">
+        <div class="wt100" :title="node.label">{{ node.label }}</div>
+      </div>
+    </template>
+  </el-tree>
+  <button @click="key">click</button>
+</template>
+
+<script lang="ts" setup>
+import {ref,nextTick} from 'vue';
+interface Tree {
+    id: number|string|null
+  label: string;
+  children?: Tree[];
+}
+const data: Tree[] = [
+  {
+    id:'0',
+    label: "哈哈哈",
+    children: [
+      {
+        id:'0',
+        label: "哈哈哈哈哈哈哈哈哈哈",
+        children: [
+          {
+            id:'0',
+            label: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+          },
+        ],
+      },
+    ],
+  },
+];
+const defaultProps = {
+  children: "children",
+  label: "label",
+};
+const handleNodeClick = (node:any) => {
+  console.log(data);
+  nodeKey = node.id
+  console.log(node,'nodeid');
+  
+};
+let nodeKey = ref('');
+function key() {
+    nodeKey = ref(String(3))
+}
+</script>
+
+<style>
+.wt100 {
+  width: 200px !important;
+  height: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
+<!-- 
+    <template #default="{ node }">     
+        <div class = "custom-tree-node" >
           <div class="wt100" :title="node.label">{{ node.label }}</div>
         </div>
-      </template>
-    </el-tree>
-  </template>
-  
-  <script lang="ts" setup>
-  interface Tree {
-    label: string
-    children?: Tree[]
-  }
-  const data: Tree[] = [
-    {
-      label: '哈哈哈',
-      children: [
-        {
-          label: '哈哈哈哈哈哈哈哈哈哈',
-          children: [
-            {
-              label: '哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
-            }
-          ]
-        }
-      ]
-    }
-  ]
-  const defaultProps = {
-    children: 'children',
-    label: 'label'
-  }
-  const handleNodeClick = (data: Tree) => {
-    console.log(data)
-  }
-  
-  </script>
-  
-  <style>
-  .wt100{
-    width: 200px !important;
-    height: 100%;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    }
-  </style>
+   -->
