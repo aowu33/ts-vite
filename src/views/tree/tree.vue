f9dc334ebe342d44ce4ddcca411fbf3990b64cdb
miao <15233722133@163.com>
2022-12-12 10:47:20 +0800

    字符串长度

--- src/views/tree/tree.vue
+++ src/views/tree/tree.vue
@@ -10,32 +10,34 @@
   >
     <template #default="{ node, data }">
       <div class="custom-tree-node" slot-scope="{ node, data }">
-        <div class="wt100" :title="node.label">{{ node.label }}</div>
+        <div class="wt100" @click="strLength(node.label)" :title="node.label">
+          {{ node.label }}
+        </div>
       </div>
     </template>
   </el-tree>
-  <input ref="input"/>
+  <input ref="input" />
   <button @click="key">click</button>
 </template>
 
 <script lang="ts" setup>
-import {ref,nextTick,onMounted} from 'vue';
+import { ref, nextTick, onMounted } from "vue";
 interface Tree {
-    id: number|string|null
+  id: number | string | null;
   label: string;
   children?: Tree[];
 }
 const data: Tree[] = [
   {
-    id:'0',
+    id: "0",
     label: "哈哈哈",
     children: [
       {
-        id:'0',
+        id: "0",
         label: "哈哈哈哈哈哈哈哈哈哈",
         children: [
           {
-            id:'0',
+            id: "0",
             label: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
           },
         ],
@@ -48,24 +50,33 @@ const defaultProps = {
   label: "label",
 };
 
-let nodeKey = ref('');
-const myTree:any = ref(null)
-const input:any = ref(null)
+let nodeKey = ref("");
+const myTree: any = ref(null);
+const input: any = ref(null);
 function key() {
-    console.log(nodeKey.value);
-    nextTick(()=>{ 
-       myTree.value.setCurrentKey(nodeKey.value) 
-    })
-    input.value.focus()
+  nextTick(() => {
+    myTree.value.setCurrentKey(nodeKey.value);
+  });
+  input.value.focus();
 }
-const handleNodeClick = (node:any) => {
-  nodeKey = ref(String(node.id))
-console.log(nodeKey.value);
-
+const handleNodeClick = (node: any) => {
+  nodeKey = ref(String(node.id));
 };
-onMounted(() => {
-    
-})
+function strLength(str: any) {
+  let len = 0;
+  for (let i = 0; i < str.length; i++) {
+    let c = str.charAt(i);
+    if (/^[\u0000-\u00ffA-Za-z1-9]+$/.test(c)) {
+      len += 1;
+    } else {
+      len += 2;
+    }
+  }
+  console.log(len);
+
+  return len;
+}
+onMounted(() => {});
 </script>
 
 <style>
